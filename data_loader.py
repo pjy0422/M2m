e5c4b735eb91ed7a7d0a1554b8dd4ff57f4a4f4d
pjy0422 <pjy0422@postech.ac.kr>
2024-04-17 21:54:43 +0900

    asf

--- data_loader.py
+++ data_loader.py
@@ -1,25 +1,23 @@
-import torch
-import os
-import numpy.random as nr
-import numpy as np
 import bisect
-from PIL import Image
+import os
 
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-from torch.utils.data.sampler import SubsetRandomSampler, WeightedRandomSampler
-from torch.utils.data import Dataset
-from torch.utils.data import TensorDataset
+import numpy as np
+import numpy.random as nr
+import torch
+from PIL import Image
 from scipy import io
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+from torch.utils.data.sampler import SubsetRandomSampler, WeightedRandomSampler
+from torchvision import datasets, transforms
 
 num_test_samples_cifar10 = [1000] * 10
 num_test_samples_cifar100 = [100] * 100
 
-DATA_ROOT = os.path.expanduser('~/data')
+DATA_ROOT = os.path.expanduser("~/data")
 
 
 def make_longtailed_imb(max_num, class_num, gamma):
-    mu = np.power(1/gamma, 1/(class_num - 1))
+    mu = np.power(1 / gamma, 1 / (class_num - 1))
     print(mu)
     class_num_list = []
     for i in range(class_num):
@@ -36,7 +34,9 @@ def get_val_test_data(dataset, num_sample_per_class, shuffle=False, random_seed=
     """
     length = dataset.__len__()
     num_sample_per_class = list(num_sample_per_class)
-    num_samples = num_sample_per_class[0] # Suppose that all classes have the same number of test samples
+    num_samples = num_sample_per_class[
+        0
+    ]  # Suppose that all classes have the same number of test samples
 
     val_list = []
     test_list = []
@@ -67,7 +67,7 @@ def get_oversampled_data(dataset, num_sample_per_class, random_seed=0):
     num_samples = list(num_sample_per_class)
 
     selected_list = []
-    indices = list(range(0,length))
+    indices = list(range(0, length))
     for i in range(0, length):
         index = indices[i]
         _, label = dataset.__getitem__(index)
@@ -87,7 +87,7 @@ def get_imbalanced_data(dataset, num_sample_per_class, shuffle=False, random_see
     length = dataset.__len__()
     num_sample_per_class = list(num_sample_per_class)
     selected_list = []
-    indices = list(range(0,length))
+    indices = list(range(0, length))
 
     for i in range(0, length):
         index = indices[i]
@@ -103,16 +103,18 @@ def get_oversampled(dataset, num_sample_per_class, batch_size, TF_train, TF_test
     print("Building {} CV data loader with {} workers".format(dataset, 8))
     ds = []
 
-    if dataset == 'cifar10':
+    if dataset == "cifar10":
         dataset_ = datasets.CIFAR10
         num_test_samples = num_test_samples_cifar10
-    elif dataset == 'cifar100':
+    elif dataset == "cifar100":
         dataset_ = datasets.CIFAR100
         num_test_samples = num_test_samples_cifar100
     else:
         raise NotImplementedError()
 
-    train_cifar = dataset_(root=DATA_ROOT, train=True, download=False, transform=TF_train)
+    train_cifar = dataset_(
+        root=DATA_ROOT, train=True, download=True, transform=TF_train
+    )
 
     targets = np.array(train_cifar.targets)
     classes, class_counts = np.unique(targets, return_counts=True)
@@ -121,7 +123,10 @@ def get_oversampled(dataset, num_sample_per_class, batch_size, TF_train, TF_test
     imbal_class_counts = [int(i) for i in num_sample_per_class]
     class_indices = [np.where(targets == i)[0] for i in range(nb_classes)]
 
-    imbal_class_indices = [class_idx[:class_count] for class_idx, class_count in zip(class_indices, imbal_class_counts)]
+    imbal_class_indices = [
+        class_idx[:class_count]
+        for class_idx, class_count in zip(class_indices, imbal_class_counts)
+    ]
     imbal_class_indices = np.hstack(imbal_class_indices)
 
     train_cifar.targets = targets[imbal_class_indices]
@@ -130,16 +135,22 @@ def get_oversampled(dataset, num_sample_per_class, batch_size, TF_train, TF_test
     assert len(train_cifar.targets) == len(train_cifar.data)
 
     train_in_idx = get_oversampled_data(train_cifar, num_sample_per_class)
-    train_in_loader = DataLoader(train_cifar, batch_size=batch_size,
-                                 sampler=WeightedRandomSampler(train_in_idx, len(train_in_idx)), num_workers=8)
+    train_in_loader = DataLoader(
+        train_cifar,
+        batch_size=batch_size,
+        sampler=WeightedRandomSampler(train_in_idx, len(train_in_idx)),
+        num_workers=8,
+    )
     ds.append(train_in_loader)
 
-    test_cifar = dataset_(root=DATA_ROOT, train=False, download=False, transform=TF_test)
+    test_cifar = dataset_(root=DATA_ROOT, train=False, download=True, transform=TF_test)
     val_idx, test_idx = get_val_test_data(test_cifar, num_test_samples)
-    val_loader = DataLoader(test_cifar, batch_size=100,
-                            sampler=SubsetRandomSampler(val_idx), num_workers=8)
-    test_loader = DataLoader(test_cifar, batch_size=100,
-                             sampler=SubsetRandomSampler(test_idx), num_workers=8)
+    val_loader = DataLoader(
+        test_cifar, batch_size=100, sampler=SubsetRandomSampler(val_idx), num_workers=8
+    )
+    test_loader = DataLoader(
+        test_cifar, batch_size=100, sampler=SubsetRandomSampler(test_idx), num_workers=8
+    )
     ds.append(val_loader)
     ds.append(test_loader)
     ds = ds[0] if len(ds) == 1 else ds
@@ -151,27 +162,35 @@ def get_imbalanced(dataset, num_sample_per_class, batch_size, TF_train, TF_test)
     print("Building CV {} data loader with {} workers".format(dataset, 8))
     ds = []
 
-    if dataset == 'cifar10':
+    if dataset == "cifar10":
         dataset_ = datasets.CIFAR10
         num_test_samples = num_test_samples_cifar10
-    elif dataset == 'cifar100':
+    elif dataset == "cifar100":
         dataset_ = datasets.CIFAR100
         num_test_samples = num_test_samples_cifar100
     else:
         raise NotImplementedError()
 
-    train_cifar = dataset_(root=DATA_ROOT, train=True, download=False, transform=TF_train)
+    train_cifar = dataset_(
+        root=DATA_ROOT, train=True, download=True, transform=TF_train
+    )
     train_in_idx = get_imbalanced_data(train_cifar, num_sample_per_class)
-    train_in_loader = torch.utils.data.DataLoader(train_cifar, batch_size=batch_size,
-                                                  sampler=SubsetRandomSampler(train_in_idx), num_workers=8)
+    train_in_loader = torch.utils.data.DataLoader(
+        train_cifar,
+        batch_size=batch_size,
+        sampler=SubsetRandomSampler(train_in_idx),
+        num_workers=8,
+    )
     ds.append(train_in_loader)
 
-    test_cifar = dataset_(root=DATA_ROOT, train=False, download=False, transform=TF_test)
-    val_idx, test_idx= get_val_test_data(test_cifar, num_test_samples)
-    val_loader = torch.utils.data.DataLoader(test_cifar, batch_size=100,
-                                                  sampler=SubsetRandomSampler(val_idx), num_workers=8)
-    test_loader = torch.utils.data.DataLoader(test_cifar, batch_size=100,
-                                                  sampler=SubsetRandomSampler(test_idx), num_workers=8)
+    test_cifar = dataset_(root=DATA_ROOT, train=False, download=True, transform=TF_test)
+    val_idx, test_idx = get_val_test_data(test_cifar, num_test_samples)
+    val_loader = torch.utils.data.DataLoader(
+        test_cifar, batch_size=100, sampler=SubsetRandomSampler(val_idx), num_workers=8
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_cifar, batch_size=100, sampler=SubsetRandomSampler(test_idx), num_workers=8
+    )
     ds.append(val_loader)
     ds.append(test_loader)
     ds = ds[0] if len(ds) == 1 else ds
@@ -201,28 +220,32 @@ def smote(data, targets, n_class, n_max):
                 row_idx = i % class_len
                 col_idx = int((i - row_idx) / class_len) % (class_len - 1)
                 new_data = np.round(
-                    lam * class_data[row_idx] + (1 - lam) * class_data[sorted_idx[row_idx, 1 + col_idx]])
+                    lam * class_data[row_idx]
+                    + (1 - lam) * class_data[sorted_idx[row_idx, 1 + col_idx]]
+                )
 
-                aug_data.append(new_data.astype('uint8'))
+                aug_data.append(new_data.astype("uint8"))
                 aug_label.append(k)
 
     return np.array(aug_data), np.array(aug_label)
 
 
-def get_smote(dataset,  num_sample_per_class, batch_size, TF_train, TF_test):
+def get_smote(dataset, num_sample_per_class, batch_size, TF_train, TF_test):
     print("Building CV {} data loader with {} workers".format(dataset, 8))
     ds = []
 
-    if dataset == 'cifar10':
+    if dataset == "cifar10":
         dataset_ = datasets.CIFAR10
         num_test_samples = num_test_samples_cifar10
-    elif dataset == 'cifar100':
+    elif dataset == "cifar100":
         dataset_ = datasets.CIFAR100
         num_test_samples = num_test_samples_cifar100
     else:
         raise NotImplementedError()
 
-    train_cifar = dataset_(root=DATA_ROOT, train=True, download=False, transform=TF_train)
+    train_cifar = dataset_(
+        root=DATA_ROOT, train=True, download=True, transform=TF_train
+    )
 
     targets = np.array(train_cifar.targets)
     classes, class_counts = np.unique(targets, return_counts=True)
@@ -231,7 +254,10 @@ def get_smote(dataset,  num_sample_per_class, batch_size, TF_train, TF_test):
     imbal_class_counts = [int(i) for i in num_sample_per_class]
     class_indices = [np.where(targets == i)[0] for i in range(nb_classes)]
 
-    imbal_class_indices = [class_idx[:class_count] for class_idx, class_count in zip(class_indices, imbal_class_counts)]
+    imbal_class_indices = [
+        class_idx[:class_count]
+        for class_idx, class_count in zip(class_indices, imbal_class_counts)
+    ]
     imbal_class_indices = np.hstack(imbal_class_indices)
 
     train_cifar.targets = targets[imbal_class_indices]
@@ -240,7 +266,9 @@ def get_smote(dataset,  num_sample_per_class, batch_size, TF_train, TF_test):
     assert len(train_cifar.targets) == len(train_cifar.data)
 
     class_max = max(num_sample_per_class)
-    aug_data, aug_label = smote(train_cifar.data, train_cifar.targets, nb_classes, class_max)
+    aug_data, aug_label = smote(
+        train_cifar.data, train_cifar.targets, nb_classes, class_max
+    )
 
     train_cifar.targets = np.concatenate((train_cifar.targets, aug_label), axis=0)
     train_cifar.data = np.concatenate((train_cifar.data, aug_data), axis=0)
@@ -248,15 +276,19 @@ def get_smote(dataset,  num_sample_per_class, batch_size, TF_train, TF_test):
     print("Augmented data num = {}".format(len(aug_label)))
     print(train_cifar.data.shape)
 
-    train_in_loader = torch.utils.data.DataLoader(train_cifar, batch_size=batch_size, shuffle=True, num_workers=8)
+    train_in_loader = torch.utils.data.DataLoader(
+        train_cifar, batch_size=batch_size, shuffle=True, num_workers=8
+    )
     ds.append(train_in_loader)
 
-    test_cifar = dataset_(root=DATA_ROOT, train=False, download=False, transform=TF_test)
+    test_cifar = dataset_(root=DATA_ROOT, train=False, download=True, transform=TF_test)
     val_idx, test_idx = get_val_test_data(test_cifar, num_test_samples)
-    val_loader = torch.utils.data.DataLoader(test_cifar, batch_size=100,
-                                             sampler=SubsetRandomSampler(val_idx), num_workers=8)
-    test_loader = torch.utils.data.DataLoader(test_cifar, batch_size=100,
-                                              sampler=SubsetRandomSampler(test_idx), num_workers=8)
+    val_loader = torch.utils.data.DataLoader(
+        test_cifar, batch_size=100, sampler=SubsetRandomSampler(val_idx), num_workers=8
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_cifar, batch_size=100, sampler=SubsetRandomSampler(test_idx), num_workers=8
+    )
     ds.append(val_loader)
     ds.append(test_loader)
     ds = ds[0] if len(ds) == 1 else ds
