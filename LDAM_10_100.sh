aaaa9b4b8cf9fff69411cdc08baffd0522c87ed0
pjy0422 <pjy0422@postech.ac.kr>
2024-04-19 15:31:36 +0900

    sg

--- LDAM_10_100.sh
+++ LDAM_10_100.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=preL_10_100                  # Job 이름
+#SBATCH --job-name=LDAM_10_100                  # Job 이름
 #SBATCH --gres=gpu:1                      # Job에 사용할 리소스 (GPU)
 #SBATCH --ntasks=8
 #SBATCH -t 24:00:00  # 24 hour              # 이 job이 리소스를 물고 있을 최대 시간
@@ -30,6 +30,6 @@ conda activate venv_312
 # module load cuda-11.8  # SPACK 세팅이 된 경우 CUDA runtime을 불러올 수 있음
 
 hostname  # 무슨 노드에 배치됐는지 표시
-python $HOME/M2m/train.py --no_over --eff_beta 0.999 -c -gen -r --loss_type LDAM   --ratio 100 --decay 2e-4 --model resnet32_norm --dataset cifar10 --lr 0.1 --batch-size 128 --name 'LDAM_M2m' --beta 0.999 --lam 0.5 --gamma 0.9 --step_size 0.1 --attack_iter 10 --warm 160 --epoch 200 --net_g /home/pjy0422/M2m/logs/Imbalance_S9916_LDAM-DRW_L0.5_W160_E0.1_I10_cifar10_R100_resnet32_norm_G0.99_B0.999/ckpt_9916.t7
+python $HOME/M2m/train.py -c -gen -r --loss_type LDAM --ratio 100 --decay 2e-4 --model resnet32_norm --dataset cifar10 --lr 0.1 --batch-size 128 --name LDAM_M2m --beta 0.999 --lam 0.5 --gamma 0.9 --step_size 0.1 --attack_iter 10 --warm 160 --epoch 200 --net_g $HOME/M2m/checkpoint/LDAM_cifar10_r100.t7
 conda deactivate
 exit 0  # explicit 하게 job을 끝내 잠재적인 completing 문제가 생기는 것을 방지
